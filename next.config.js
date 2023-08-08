334107d58e5546fb46e6cba24da94502be7c2f22
Marisol <marisol.azofeifa@totemtech.net>
2023-08-08 17:28:41 -0600

    try

--- next.config.js
+++ next.config.js
@@ -2,11 +2,17 @@
 const path = require('path')
 const nextConfig = {
   output: 'export',
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.output.publicPath = 'https://hatchcloud.github.io/animation_mask/';
+    }
+    return config;
+  },
   swcMinify: true,
-  images: { unoptimized: true },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: { unoptimized: true }
 }
 
 module.exports = nextConfig
